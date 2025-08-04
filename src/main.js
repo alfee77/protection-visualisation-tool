1f51e87089f357321011dac3803afb2393c84e9b
Alistair Peat <alistair.peat@gmail.com>
2025-08-04 15:16:46 +0100

    removed "public" from fetch url

--- src/main.js
+++ src/main.js
@@ -21,26 +21,26 @@ let chartCanvas = document.querySelector("#chart");
  * The following fetch calls "get" the network buses and AC line data.
  *
  */
-fetch(new Request("./public/ETYS DATA 2024 YEAR 2 Buses.json"))
+fetch(new Request("./ETYS DATA 2024 YEAR 2 Buses.json"))
   .then((response) => response.json())
   .then((data) => {
     modelBuses = data.filter((dat) => dat["Owner Name"] === "SHET");
     sortByKey(modelBuses, "Bus  Name");
   });
 
-fetch(new Request("./public/ETYS DATA 2024 YEAR 2 AC Lines.json"))
+fetch(new Request("./ETYS DATA 2024 YEAR 2 AC Lines.json"))
   .then((response) => response.json())
   .then((data) => {
     modelBranches = data;
   });
 
-fetch(new Request("./public/ETYS DATA 2024 YEAR 2 2 Winding Tx.json"))
+fetch(new Request("./ETYS DATA 2024 YEAR 2 2 Winding Tx.json"))
   .then((response) => response.json())
   .then((data) => {
     model2wtx = data;
   });
 
-fetch(new Request("./public/ETYS DATA 2024 YEAR 2 3 Winding Tx.json"))
+fetch(new Request("./ETYS DATA 2024 YEAR 2 3 Winding Tx.json"))
   .then((response) => response.json())
   .then((data) => {
     model3wtx = data;
