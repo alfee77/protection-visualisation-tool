2ad356ae38713ae0275fb50df6efae87cdaca7aa
Alistair Peat <alistair.peat@gmail.com>
2026-01-05 20:58:01 +0000

    ??

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,17 @@
-export default {
-  // config options
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
   base: "/protection-visualisation-tool/",
-};
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        circuit_builder: resolve(__dirname, "circuit_builder/index.html"),
+      },
+    },
+  },
+});
