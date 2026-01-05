a620c1e56d1b5ce63a465738f2a59f73dba0ba60
Alistair Peat <alistair.peat@gmail.com>
2026-01-05 21:15:04 +0000

    !!!

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 export default defineConfig({
   base: "/protection-visualisation-tool/",
